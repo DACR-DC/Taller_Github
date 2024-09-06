addf6c766b8b6fc1b47522a2a4acbd42642ca639
jdcacaos <jcacaos1@miumg.edu.gt>
2024-09-06 17:09:43 -0600

    Hoy si los cambios de diseño y cellclick

--- CapaVista/Form1.Designer.cs
+++ CapaVista/Form1.Designer.cs
@@ -29,19 +29,192 @@ namespace CapaVista
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnconsultar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnactualizar = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.cbestado = new System.Windows.Forms.ComboBox();
+            this.txtdepto = new System.Windows.Forms.TextBox();
+            this.txtpuesto = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnconsultar
+            // 
+            this.btnconsultar.Location = new System.Drawing.Point(783, 59);
+            this.btnconsultar.Name = "btnconsultar";
+            this.btnconsultar.Size = new System.Drawing.Size(181, 118);
+            this.btnconsultar.TabIndex = 27;
+            this.btnconsultar.Text = "Consultar";
+            this.btnconsultar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(618, 140);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(150, 37);
+            this.btneliminar.TabIndex = 26;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnactualizar
+            // 
+            this.btnactualizar.Location = new System.Drawing.Point(618, 97);
+            this.btnactualizar.Name = "btnactualizar";
+            this.btnactualizar.Size = new System.Drawing.Size(150, 37);
+            this.btnactualizar.TabIndex = 25;
+            this.btnactualizar.Text = "Actualizar";
+            this.btnactualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(618, 56);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(150, 37);
+            this.btnguardar.TabIndex = 24;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            // 
+            // cbestado
+            // 
+            this.cbestado.FormattingEnabled = true;
+            this.cbestado.Location = new System.Drawing.Point(176, 146);
+            this.cbestado.Name = "cbestado";
+            this.cbestado.Size = new System.Drawing.Size(371, 24);
+            this.cbestado.TabIndex = 23;
+            // 
+            // txtdepto
+            // 
+            this.txtdepto.Location = new System.Drawing.Point(176, 117);
+            this.txtdepto.Name = "txtdepto";
+            this.txtdepto.Size = new System.Drawing.Size(371, 22);
+            this.txtdepto.TabIndex = 22;
+            // 
+            // txtpuesto
+            // 
+            this.txtpuesto.Location = new System.Drawing.Point(176, 87);
+            this.txtpuesto.Name = "txtpuesto";
+            this.txtpuesto.Size = new System.Drawing.Size(371, 22);
+            this.txtpuesto.TabIndex = 21;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(176, 56);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(371, 22);
+            this.txtnombre.TabIndex = 20;
+            // 
+            // dgv1
+            // 
+            this.dgv1.AllowUserToAddRows = false;
+            this.dgv1.AllowUserToDeleteRows = false;
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(12, 197);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.ReadOnly = true;
+            this.dgv1.RowHeadersWidth = 51;
+            this.dgv1.RowTemplate.Height = 24;
+            this.dgv1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv1.Size = new System.Drawing.Size(974, 258);
+            this.dgv1.TabIndex = 19;
+            this.dgv1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellContentClick);
+            this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Estado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Departamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Puesto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(454, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "CRUD EMPLEADOS";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(998, 472);
+            this.Controls.Add(this.btnconsultar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnactualizar);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.cbestado);
+            this.Controls.Add(this.txtdepto);
+            this.Controls.Add(this.txtpuesto);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "CRUD";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnconsultar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnactualizar;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.ComboBox cbestado;
+        private System.Windows.Forms.TextBox txtdepto;
+        private System.Windows.Forms.TextBox txtpuesto;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- CapaVista/Form1.cs
+++ CapaVista/Form1.cs
@@ -16,5 +16,15 @@ namespace CapaVista
         {
             InitializeComponent();
         }
+
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dgv1.Rows[e.RowIndex];
+
+            txtnombre.Text = row.Cells["nombre_completo"].Value.ToString();
+            txtpuesto.Text = row.Cells["puesto"].Value.ToString();
+            txtdepto.Text = row.Cells["departamento"].Value.ToString();
+            cbestado.SelectedItem = row.Cells["estado"].Value.ToString();
+        }
     }
 }
