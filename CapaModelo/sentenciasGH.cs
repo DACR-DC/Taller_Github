5ed9bdf2131e1ec4af97bb6aada4c12cb6bae1a8
JvCas10 <jcastellanosp3@miumg.edu.gt>
2024-09-06 17:38:59 -0600

    Agregando boton de guardar y algunas clases

---
+++ CapaModelo/sentenciasGH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+    public class sentenciasGH
+    {
+        ConexionGH con = new ConexionGH();
+
+        public void guardarEmpleado(string nombre_completo, string puesto, string departamento, int estado)
+        {
+            string query = $"INSERT INTO empleados (nombre_completo, Puesto, Departamento, Estado) VALUES ('{nombre_completo}', '{puesto}', '{departamento}', '{estado}')";
+            OdbcCommand cmd = new OdbcCommand(query, con.conexion());
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
